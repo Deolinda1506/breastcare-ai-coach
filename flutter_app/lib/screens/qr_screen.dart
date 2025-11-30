8b98f6777474387ea561cbff86277a964990edb5
Gnon Déolinda Bio Bogore <139719649+Deolinda1506@users.noreply.github.com>
2025-12-01 01:29:15 +0200

    Update qr_screen.dart

--- flutter_app/lib/screens/qr_screen.dart
+++ flutter_app/lib/screens/qr_screen.dart
@@ -2,15 +2,22 @@ import 'package:flutter/material.dart';
 import 'package:qr_flutter/qr_flutter.dart';
 
 class QrScreen extends StatelessWidget {
+  final String data;
+
+  const QrScreen({super.key, required this.data});
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("QR Code Test")),
+      appBar: AppBar(
+        title: const Text("QR Code"),
+      ),
       body: Center(
-        child: QrImage(
-          data: "HELLO-WORLD-TEST",
+        child: QrImageView(
+          data: data,
           version: QrVersions.auto,
           size: 250,
+          backgroundColor: Colors.white,
         ),
       ),
     );
